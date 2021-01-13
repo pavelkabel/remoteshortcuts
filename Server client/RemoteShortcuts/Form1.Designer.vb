208056a73cbd3ecf499ffc77177b3730a613c67d
pája <76393432+pavelkabel@users.noreply.github.com>
2021-01-13 13:13:09 +0100

    Add files via upload

---
+++ Server client/RemoteShortcuts/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Process1 = New System.Diagnostics.Process()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Process1
+        '
+        Me.Process1.StartInfo.Domain = ""
+        Me.Process1.StartInfo.LoadUserProfile = False
+        Me.Process1.StartInfo.Password = Nothing
+        Me.Process1.StartInfo.StandardErrorEncoding = Nothing
+        Me.Process1.StartInfo.StandardInputEncoding = Nothing
+        Me.Process1.StartInfo.StandardOutputEncoding = Nothing
+        Me.Process1.StartInfo.UserName = ""
+        Me.Process1.SynchronizingObject = Me
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(165, 0)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "RemoteShortcuts"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Process1 As Process
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Server client/RemoteShortcuts/Form1.vb
@@ -0,0 +1,47 @@
+﻿Imports System.ComponentModel
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Process1.Start()
+    End Sub
+
+    Public Shared a As Integer = 0
+    Private Sub checkNodeServerRunning()
+        For Each clsProcess As Process In Process.GetProcesses()
+            If clsProcess.ProcessName.StartsWith("node") Then
+                a += 1
+            End If
+        Next
+        If (a >= 1) Then
+            Timer1.Interval = 1000
+            a = 0
+            If (a > 5) Then
+                Process1.Kill()
+            End If
+        Else
+            Process1.Start()
+            Timer1.Interval = 2000
+        End If
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        checkNodeServerRunning()
+    End Sub
+
+    Public Sub Killer(processName As String)
+        Dim psi As ProcessStartInfo = New ProcessStartInfo
+        psi.Arguments = "/im " & processName & " /f"
+        psi.FileName = "taskkill"
+        Dim p As Process = New Process()
+        p.StartInfo = psi
+        p.Start()
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        If MessageBox.Show("are u sure to close this application?", "Close", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = Windows.Forms.DialogResult.Yes Then
+            Killer("node.exe")
+        Else
+            e.Cancel = True
+        End If
+    End Sub
+
+End Class
